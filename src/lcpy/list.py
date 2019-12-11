c7f45ccb21826639e7afb011f2c3d9ddd1e7eb9d
Nicky Huang <nickyfoto@gmail.com>
2019-12-11 15:10:06 -0500

    make List class subscriptable

--- src/lcpy/list.py
+++ src/lcpy/list.py
@@ -1,2 +1,8 @@
-class List:
+class MetaCls(type):
+    def __getitem__(cls, index):
+        pass
+       # print("Using meta __getitem__ on classes that have my type")
+
+class List(metaclass=MetaCls):
     pass
+
