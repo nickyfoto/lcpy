63e3028fe39010bc9904d4df852c3972508bc792
Nicky Huang <nickyfoto@gmail.com>
2020-01-23 23:54:40 -0500

    linked_list

--- src/lcpy/linked_list.py
+++ src/lcpy/linked_list.py
@@ -1,9 +1,11 @@
 class ListNode:
+    
     def __init__(self, x):
         self.val = x
         self.next = None
+
     def __str__(self):
-        # print(self.val)
+        
         l = [self.val]
         n = self
         while n.next:
@@ -11,8 +13,27 @@ class ListNode:
             n = n.next
         return str(l)
 
+    def reverse(self): 
+        prev = None
+        current = self 
+        while current: 
+            next_node = current.next
+            current.next = prev 
+            prev = current 
+            current = next_node
+        self = prev 
+
+    def __eq__(self, other):
+        node = self
+        while node and other:
+            if node.val != other.val:
+                return False
+            node = node.next
+            other = other.next
+        return True
 
 # l = [2,7,5,8,8,8]
+
 def build_head(l):
     head = l.pop(0)
     head = ListNode(head) 
@@ -22,3 +43,31 @@ def build_head(l):
         n.next = ListNode(l.pop(0))
         n = n.next
     return head
+
+
+if __name__ == '__main__':
+    pass
+    # create a linked list with a cycle in it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
