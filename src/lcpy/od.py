19d375ddb564a0ac32915a875ce7fcc3a17710e0
Qiang Huang <nickyfoto@gmail.com>
2020-02-10 13:43:01 -0500

    od

--- src/lcpy/od.py
+++ src/lcpy/od.py
@@ -3,10 +3,10 @@ OrderedCounter
 """
 from collections import OrderedDict, Counter
 class OrderedCounter(Counter, OrderedDict):
-     'Counter that remembers the order elements are first encountered'
+    'Counter that remembers the order elements are first encountered'
 
-     def __repr__(self):
-         return '%s(%r)' % (self.__class__.__name__, OrderedDict(self))
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, OrderedDict(self))
 
-     def __reduce__(self):
-         return self.__class__, (OrderedDict(self),)
+    def __reduce__(self):
+        return self.__class__, (OrderedDict(self),)
